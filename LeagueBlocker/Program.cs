9fa89dd5f26acb7c03ae664954aa14439f52a5bc
udaceue <rjemik9@gmail.com>
2023-11-30 15:50:45 +0100

    Initial commit

---
+++ LeagueBlocker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+public class LeagueBlocker
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            var process = Process.GetProcessesByName("RiotClientUx");
+
+            if (process.Length > 0)
+            {
+                process[0].Kill();
+                Console.WriteLine("Destroyed Riot Client");
+            }
+            Console.WriteLine("Searching For Riot Client");
+            Thread.Sleep(15000); // Check every 15second
+        }
+    }
+}
